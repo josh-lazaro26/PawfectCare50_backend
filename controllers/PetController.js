32fe7cc74d3a859254ce43bd9525fd67eeb83a14
josh-lazaro26 <joshualazaro262@gmail.com>
2025-11-09 02:36:32 +0800

    For Deployment

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -83,7 +83,6 @@ exports.addPet = async (req, res) => {
         Name: ${name || "unknown"}
         Breed: ${breed || "unknown"}
         Size: ${size || "unknown"}
-        Image: ${image || "unknown"}
         Color: ${color || "unknown"}
       `;
 
